8a07c35a0d9d119fe5688ed4e926b0fb9bc89f1a
Ekaterina Markova <ivanova_katrin@mail.ru>
2022-09-03 12:55:22 +0300

    Решение задачи №4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 2; // первый пример "Нахождение максимального из трех чисел"
+int b = 3;
+int c = 7;
+int max = a;
+if (b>max) max = b;
+if (c>max) max =c;
+Console.WriteLine(max);
+
+int A = 44; // второй пример "Нахождение максимального из трех чисел"
+int B = 5;
+int C = 78;
+int max1= A;
+if (B>max1) max1 = B;
+if (C>max1) max1 =C;
+Console.WriteLine(max1);
+
+int x = 22; // третий пример "Нахождение максимального из трех чисел"
+int y = 3;
+int z = 9;
+int max2 = x;
+if (y>max2) max2 = y;
+if (z>max2) max2 =z;
+Console.WriteLine(max2);
