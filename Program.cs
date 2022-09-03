8109c446dabb0ad5c662a1a8aafca32695f1f1c7
Ekaterina Markova <ivanova_katrin@mail.ru>
2022-09-03 12:32:44 +0300

    Решение задачи №2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5; // первый пример (нахождение максимального и минимального значения)
+int b = 7;
+int max = a;
+int min = b;
+if (a>b) max = a; min = b;
+if (b>a) max = b; min = a;
+Console.WriteLine (max);
+Console.WriteLine (min);
+
+int c = 2; // второй пример (нахождение максимального и минимального значения)
+int d = 10;
+int max1 = c;
+int min1 = d;
+if (c>d) max1 = c; min1 = d;
+if (d>c) max1 = d; min1 = c;
+Console.WriteLine (max1);
+Console.WriteLine (min1);
+
+int A = -9; // третий пример (нахождение максимального и минимального значения)
+int B = -3;
+int max2= A;
+int min2 = B;
+if (A>B) max2 = A; min2 = B;
+if (B>A) max2 = B; min2 = A;
+Console.WriteLine (max2);
+Console.WriteLine (min2);
