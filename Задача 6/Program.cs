8faa6e1c517fde1c93d5f607070dc5b37b3bdb1b
Ekaterina Markova <ivanova_katrin@mail.ru>
2022-09-03 15:00:12 +0300

    Решение задачи №6

---
+++ Задача 6/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 4; // первый пример "Является ли числом четным"
+if (a%2 == 0)
+   Console.WriteLine("да");
+else 
+   Console.WriteLine("нет");
+
+
+int b = -3; // второй пример "Является ли числом четным"
+if (b%2 == 0)
+   Console.WriteLine("да");
+else 
+   Console.WriteLine("нет");
+
+
+   int c = 7; // третий пример "Является ли числом четным"
+if (c%2 == 0)
+   Console.WriteLine("да");
+else 
+   Console.WriteLine("нет");
