f305b43043bfd45615ff1303beaba77916a51c42
Ekaterina Markova <ivanova_katrin@mail.ru>
2022-09-05 13:09:59 +0300

    Решение задачи №13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int NumberA = 645; // первый пример
+Console.WriteLine(NumberA);
+if (NumberA >99)
+Console.WriteLine(Convert.ToString(NumberA)[2]);
+else
+{
+    Console.WriteLine("третьей цифры нет"); 
+
+}
+    
+
+int NumberB = 78; // второй пример
+Console.WriteLine(NumberB);
+if (NumberB >99)
+Console.WriteLine(Convert.ToString(NumberB)[2]);
+else
+{
+    Console.WriteLine("третьей цифры нет"); 
+
+}
+   
+
+int NumberC = 32679; // третий пример
+Console.WriteLine(NumberC);
+if (NumberC >99)
+Console.WriteLine(Convert.ToString(NumberC)[2]);
+else
+{
+    Console.WriteLine("третьей цифры нет"); 
+
+}
