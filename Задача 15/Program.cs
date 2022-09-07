d074c95c4165b67c4c04f5d0c7dab49fef6ea76e
Ekaterina Markova <ivanova_katrin@mail.ru>
2022-09-07 08:38:43 +0300

    Решение задачи №15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,8 @@
+﻿int Day = Convert.ToInt32(Console.ReadLine());
+int Max = 7;
+if (Day == 6||Day == 7)
+Console.WriteLine("да");
+if (Day == 1||Day == 2||Day == 3||Day == 4||Day == 5)
+Console.WriteLine("нет");
+if (Day >Max)
+Console.WriteLine("Введите число от 1 до 7: ");
